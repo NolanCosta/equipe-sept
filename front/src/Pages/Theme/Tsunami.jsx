87ff559ea356ac181c69ddb13f8da353cef0f77f
YanaMlnk <yanamelnyk1998@gmail.com>
2024-11-07 13:36:13 +0100

    new components created

--- front/src/Pages/Theme/Tsunami.jsx
+++ front/src/Pages/Theme/Tsunami.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import CardTheme from "../../Components/Cards/CardTheme";
 
 function Tsunami() {
   return (
     <>
-      <h2>Tsunami</h2>
+      <div className="pt-20">
+        <h1 className="text-[#B1B18D]">Tsunami</h1>
+        <p>Un tsunami est une énorme vague de mer très puissante qui se forme après un tremblement de terre sous l'eau et qui peut inonder les plages et les villes.</p>
+        <div className="w-full h-[calc(100vh-78px-4rem)] flex flex-row justify-around items-center">
+          <CardTheme title="MAISON" onClick={() => navigate("/tsunami")} />
+          <CardTheme title="EXTÉRIEUR" onClick={() => navigate("/tremblement")} />
+        </div>
+      </div>
     </>
   );
 }
