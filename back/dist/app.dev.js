208aff5b03723fec13f6942870c0056e920cd980
evarazzini <eva.razzini@epitech.eu>
2024-11-07 15:40:35 +0100

    add access control

--- back/dist/app.dev.js
+++ back/dist/app.dev.js
@@ -10,16 +10,14 @@ var cookieParser = require('cookie-parser');
 
 var logger = require('morgan');
 
+var cors = require('cors');
+
 var indexRouter = require('./routes/index');
 
 var usersRouter = require('./routes/users');
 
 var app = express();
-var port = process.env.PORT || 5000;
-app.set('port', port);
-app.listen(port, function () {
-  console.log("Serveur d\xE9marr\xE9 et \xE9coutant sur le port ".concat(port));
-});
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({
@@ -27,19 +25,23 @@ app.use(express.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express["static"](path.join(__dirname, 'public')));
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 app.use('/', indexRouter);
-app.use('/users', usersRouter); // catch 404 and forward to error handler
-
+app.use('/users', usersRouter);
 app.use(function (req, res, next) {
   next(createError(404));
-}); // error handler
-
+});
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {}; // render the error page
-
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    error: res.locals.message
+  });
 });
 module.exports = app;
--- back/models/dist/QuestionInondation.dev.js
+++ back/models/dist/QuestionInondation.dev.js
@@ -13,5 +13,23 @@ var questions = [{
   question: "Une alerte est donnée sur la radio, que faire?",
   choices: ["Prévenir ses parents ?", "Continuer de dessiner ?"],
   answer: "Prevenir ses parents ?"
+}, {
+  id: 3,
+  image: "https://media.discordapp.net/attachments/1303701971294883866/1304065630948823111/story-inon3.webp?ex=672e0942&is=672cb7c2&hm=3cd44a11ad96e838f956973d79045dafc2c92b42137e63386cb7a71dfb8d8753&=&format=webp&width=538&height=538",
+  question: "Votre enfant vous avertit d’une alerte inondation donnée à la radio, que faites-vous ?",
+  choices: ["continuer de faire ce que l'on fait", "Vérifier l'information, se préparer. On ne sait jamais."],
+  answer: "Vérifier l'information, se préparer. On ne sait jamais."
+}, {
+  id: 4,
+  image: "https://media.discordapp.net/attachments/1303701971294883866/1304065631464718347/story-inon4.webp?ex=672e0942&is=672cb7c2&hm=41af7bfec563b927f71d6283b81bf933546b5c08d771270e173256c57d04ef67&=&format=webp&width=538&height=538",
+  question: "Quoi mettre dans son sac d’evacuation ?",
+  choices: ["Nourriture/eau, lampe à pile, vêtement chaud, etc...", "GameBoy, skateboard, trousse d'école, ect ..."],
+  answer: "Nourriture/eau, lampe à pile, vêtement chaud, etc..."
+}, {
+  id: 5,
+  image: "https://media.discordapp.net/attachments/1303701971294883866/1304065632043663400/story-inon5.webp?ex=672e0943&is=672cb7c3&hm=c148e31af3fdeb3878c7b17f9d598c8f94077049b9dd1293545c119b2a8ebdb4&=&format=webp&width=538&height=538",
+  question: "Quoi mettre dans son sac d’evacuation ?",
+  choices: ["Nourriture/eau, lampe à pile, vêtement chaud, etc...", "GameBoy, skateboard, trousse d'école, ect ..."],
+  answer: "Nourriture/eau, lampe à pile, vêtement chaud, etc..."
 }];
 module.exports = questions;
