12cab8c71c25568dc280a3bdd170d256c3c08b3d
YanaMlnk <yanamelnyk1998@gmail.com>
2024-11-07 17:50:53 +0100

    navigation

--- front/src/Pages/Theme/Tremblement.jsx
+++ front/src/Pages/Theme/Tremblement.jsx
@@ -9,7 +9,7 @@ function Tremblement() {
     <>
       <div className="pt-20">
         <h1 className="text-[#B1B18D]">Tremblement</h1>
-        <p>Un tremblement de terre est quand le sol tremble ou bouge très fort à cause de mouvements sous la terre, et cela peut casser des bâtiments et créer des fissures.</p>
+        <p className = "text-2xl text-black">Un tremblement de terre est quand le sol tremble ou bouge très fort à cause de mouvements sous la terre, et cela peut casser des bâtiments et créer des fissures.</p>
         <div className="w-full h-[calc(100vh-78px-4rem)] flex flex-row justify-around items-center">
           <CardTheme title="MAISON" onClick={() => navigate("/tremblement/maison")} />
           <CardTheme title="EXTÉRIEUR" onClick={() => navigate("/tremblement/exterieur")} />
