41305a0776ad3e7e60cc729e60d3120283a13062
NolanCosta <nolan.costa@epitech.eu>
2024-11-08 08:55:06 +0100

    edit story

--- front/src/Components/Story/Story.jsx
+++ front/src/Components/Story/Story.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
+import ProgressBar from "../ProgressBar/ProgressBar";
 
 function Story({ theme }) {
   const [story, setStory] = useState(null);
   const [advencement, setAdvencement] = useState(1);
-  console.log(story);
 
   const getStory = async () => {
     try {
@@ -16,11 +16,7 @@ function Story({ theme }) {
   };
 
   const checkAnswer = (answer) => {
-    console.log(answer);
-    console.log(story[advencement - 1].answer);
     if (answer === story[advencement - 1].answer) {
-      console.log("good answer");
-
       setAdvencement(advencement + 1);
     }
   };
@@ -29,27 +25,39 @@ function Story({ theme }) {
     return story.map((question) => {
       if (question.id === advencement) {
         return (
-          <div key={question.id}>
-            <h1>{question.text}</h1>
-            <img src={question.image} alt="question image" />
+          <div
+            key={question.id}
+            className="h-[calc(100vh-78px-4rem)] flex flex-col justify-between items-center"
+          >
+            <ProgressBar size={story.length} advencement={advencement} />
+            <h1 className="h-1/6 flex items-center text-black">
+              {question.text}
+            </h1>
+            <img
+              className="w-1/2 h-3/6"
+              src={question.image}
+              alt="question image"
+            />
             {question.question ? (
-              <div>
-                <p className="text-black">{question.question}</p>
-                {question.choices.map((choice) => {
-                  return (
-                    <button
-                      className="bg-white text-black"
-                      onClick={() => {
-                        checkAnswer(choice);
-                      }}
-                    >
-                      {choice}
-                    </button>
-                  );
-                })}
+              <div className="h-1/6 flex flex-col justify-around items-center">
+                <h2 className="text-3xl text-black">{question.question}</h2>
+                <div className="flex flex-row items-center gap-10">
+                  {question.choices.map((choice) => {
+                    return (
+                      <button
+                        className="bg-white text-black"
+                        onClick={() => {
+                          checkAnswer(choice);
+                        }}
+                      >
+                        {choice}
+                      </button>
+                    );
+                  })}
+                </div>
               </div>
             ) : (
-              <div>
+              <div className="h-1/6 flex items-center">
                 <button
                   onClick={() => {
                     setAdvencement(advencement + 1);
@@ -69,7 +77,7 @@ function Story({ theme }) {
     getStory();
   }, [theme]);
 
-  return <div>{story && renderQuestion()}</div>;
+  return <>{story && renderQuestion()}</>;
 }
 
 export default Story;
