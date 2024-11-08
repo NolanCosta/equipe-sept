74aec47abb3adb5c6706d9f7a72e635e3eda0f0e
evarazzini <eva.razzini@epitech.eu>
2024-11-08 03:03:34 +0100

    add title, css

--- front/src/Pages/Theme/Innondation.jsx
+++ front/src/Pages/Theme/Innondation.jsx
@@ -8,8 +8,9 @@ function Innondation() {
   return (
     <>
       <div className="pt-20">
-        <h1 className="text-[#B1B18D]">Innondation</h1>
-        <p className = "text-2xl text-black">Une inondation est quand il y a trop d'eau qui déborde des rivières ou de la mer et recouvre la terre, les rues et parfois les maisons.</p>
+        <h1 className="text-[#b1b18d]">Innondation</h1>
+        <p className = "text-2xl text-[#b1b18d]">Une inondation, c'est quand il y a de fortes pluies ou quand trop d'eau déborde des rivières<br/> ou de la mer et recouvre la terre,
+        Les rues et parfois les maisons.</p>
         <div className="w-full h-[calc(100vh-78px-4rem)] flex flex-row justify-around items-center">
           <CardTheme title="MAISON" onClick={() => navigate("/innondation/maison")} />
           <CardTheme title="EXTÉRIEUR" onClick={() => navigate("/innondation/exterieur")} />
